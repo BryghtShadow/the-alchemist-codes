5b6fa0f9e8eb056366f5af94606325e486ec38e2
BryghtShadow <bryghtshadow@gmail.com>
2020-10-13 03:02:36 +1200

    fantom -> py (wip)

--- vanilla.py
+++ vanilla.py
@@ -1,4 +1,10 @@
+import base64
+import csv
+import json
 import sqlite3
+from collections import OrderedDict
+from io import StringIO
+from typing import Dict, Any, Optional, Mapping, Callable
 from urllib.parse import quote_plus
 
 import requests
@@ -8,11 +14,13 @@ class Vanilla:
     cache_name = 'cache.sqlite'
     api_base = 'https://gitlab.com/api/v4'
 
+    def __init__(self):
+        self.conn = sqlite3.connect(self.cache_name)
+        self.conn.row_factory = sqlite3.Row
+
     def init_cache(self):
-        conn = sqlite3.connect(self.cache_name)
-        conn.row_factory = sqlite3.Row
-        with conn:
-            conn.execute('''CREATE TABLE IF NOT EXISTS namespaces(
+        with self.conn:
+            self.conn.execute('''CREATE TABLE IF NOT EXISTS namespaces(
                 id INT,
                 name TEXT,
                 path TEXT,
@@ -20,23 +28,21 @@ class Vanilla:
                 full_path TEXT,
                 PRIMARY KEY(id)
             );''')
-            conn.execute('''CREATE TABLE IF NOT EXISTS projects(
+            self.conn.execute('''CREATE TABLE IF NOT EXISTS projects(
                 id INT,
                 name TEXT,
                 path TEXT,
                 namespace_id INT,
                 PRIMARY KEY(id)
             );''')
-            conn.execute('''CREATE TABLE IF NOT EXISTS files(
+            self.conn.execute('''CREATE TABLE IF NOT EXISTS files(
                 project_id INT,
                 file_path TEXT,
                 content_sha256 TEXT,
-                commit_id TEXT,
                 last_commit_id TEXT,
                 content TEXT,
                 PRIMARY KEY(project_id, file_path)
             );''')
-        conn.close()
         # init namespaces
         group_name = 'the-alchemist-codes'
         ns = self.get_namespace(group_name)
@@ -71,9 +77,8 @@ class Vanilla:
         return v
 
     def get_namespace(self, path):
-        conn = sqlite3.connect(self.cache_name)
-        with conn:
-            n = conn.execute('''
+        with self.conn:
+            n = self.conn.execute('''
                 SELECT n.id AS id,
                        n.name AS name,
                        n.path AS path,
@@ -83,7 +88,6 @@ class Vanilla:
                  WHERE n.kind="group"
                    AND n.path=:path;
             ''', {'path': path})
-        conn.close()
         return n
 
     def init_project(self, group_name, project_name):
@@ -97,13 +101,11 @@ class Vanilla:
             'path': data['path'],
             'group_id': data['namespace']['id'],
         }
-        conn = sqlite3.connect(self.cache_name)
-        with conn:
-            conn.execute('''
+        with self.conn:
+            self.conn.execute('''
                 INSERT INTO projects(id, name, path, namespace_id)
                 VALUES(:id, :name, :path, :group_id);
             ''', p)
-        conn.close()
         return p
 
     def get_project(self, group_name, project_name):
@@ -122,13 +124,265 @@ class Vanilla:
         conn.close()
         return p
 
+    def get_file(self, project_name, file_path, force_update=False):
+        project = self.conn.execute('''
+        SELECT p.id AS id,
+               p.name AS name,
+               p.path AS path
+          FROM projects AS p
+         WHERE p.path = :path COLLATE NOCASE;
+        ''', {
+            'path': project_name,
+        }).fetchone()
 
-if __name__ == '__main__':
+        p_id = project['id']
+        url = f'{self.api_base}/projects/{p_id}/repository/files/{quote_plus(file_path)}'
+        resp = requests.head(url, params={'ref': 'master'})
+        # header_keys = [
+        #     'x-gitlab-blob-id',
+        #     'x-gitlab-commit-id',
+        #     'x-gitlab-content-sha256',
+        #     'x-gitlab-encoding',
+        #     'x-gitlab-file-name',
+        #     'x-gitlab-file-path',
+        #     'x-gitlab-last-commit-id',
+        #     'x-gitlab-ref',
+        #     'x-gitlab-size',
+        # ]
+
+        f = self.conn.execute('''
+            SELECT f.project_id AS project_id,
+                   f.file_path AS file_path,
+                   f.content_sha256 AS content_sha256,
+                   f.last_commit_id AS last_commit_id,
+                   f.content AS content
+              FROM files as f
+             WHERE f.project_id = :project_id
+               AND f.file_path = :file_path COLLATE NOCASE
+               AND f.last_commit_id = :last_commit_id;
+        ''', {
+            'project_id': p_id,
+            'file_path': resp.headers.get('x-gitlab-file-path'),
+            'last_commit_id': resp.headers.get('x-gitlab-last-commit-id'),
+        }).fetchone()
+        if f is None or force_update:
+            resp = requests.get(url, {'ref': 'master'})
+            data = resp.json()
+            content = base64.b64decode(data['content']).decode('utf-8')
+            f = {
+                'project_id': p_id,
+                'file_path': data['file_path'],
+                'content_sha256': data['content_sha256'],
+                'last_commit_id': data['last_commit_id'],
+                'content': content,
+            }
+            with self.conn:
+                self.conn.execute('''
+                INSERT INTO files (
+                    project_id,
+                    file_path,
+                    content_sha256,
+                    last_commit_id,
+                    content
+                ) VALUES (
+                    :project_id,
+                    :file_path,
+                    :content_sha256,
+                    :last_commit_id,
+                    :content
+                ) ON CONFLICT (project_id, file_path) DO UPDATE SET
+                content_sha256=excluded.content_sha256,
+                last_commit_id=excluded.last_commit_id,
+                content=excluded.content;''', f)
+        return f['content']
+
+
+def main():
     vanilla = Vanilla()
     vanilla.init_cache()
 
-    # conn = sqlite3.connect('cache.sqlite')
-    # conn.row_factory = sqlite3.Row
+    languages = (
+        'english',
+        'french',
+        'german',
+        'spanish',
+    )
+    toReturn = {}
+    for lang in languages:
+        file_path = f'Loc/{lang}/LocalizedMasterParam'
+        content = vanilla.get_file('Global', file_path)
+        reader = csv.reader(content, delimiter='\t')
+    jpConceptCardLoc = vanilla.get_file('Global', 'Loc/japanese/external_conceptcard')
+
+    def parse(remotePath, server):
+        content = vanilla.get_file(remotePath, 'Data/MasterParam')
+        gObj = json.loads(content)
+        return  gObj
+
+    mp = MasterParam()
+    wikiFiles = mp.generate({
+        server: parse(remotePath, server)
+        for server, remotePath in {
+            'jp': 'Japan',
+            'gl': 'Global',
+        }.items()
+    })
+
+
+class GameFile:
+    def parse(self, server, remotePath):
+        pass
+
+
+class JsonGameFile(GameFile):
+    def __init__(self, endpoint):
+        super(JsonGameFile, self).__init__('Data/'+endpoint)
+
+    def _parse(self, s):
+        return json.loads(s)
+
+class MasterParam(JsonGameFile):
+    def parse(self, server, remotePath):
+        result = {
+            'data': super(MasterParam, self).parse(server, remotePath),
+        }
+
+
+    def generate(self, data):
+        to_return = Dict[str, str]
+
+        # Extract loc
+        data = self.subdivide(data, (lambda k, v=None: k))
+        loc: Dict[str, Dict[str, str]] = {}
+        loc.update(data['loc'].get('gl', {}))
+        loc.update(data['loc'].get('jp', {}))
+        data = data['data']
+
+        def get_loc_for(key: str, jp_val: Optional[str]) -> Mapping[str, str]:
+            m = {
+                k: v.pop(key)
+                for k, v in loc.items()
+                if v.get(key) is not None
+            }
+            if jp_val is not None:
+                m['japanese'] = jp_val
+            return m
+
+        # Group by key
+        groups = self.subdivide(data, (lambda k, v: k))
+
+        def get_iname(m: Mapping[str, Optional[str]]) -> str:
+            return m.get('iname')
+
+        def store_packet(group: str,
+                         loc_keys: Dict[str, str],
+                         key: str,
+                         packet: Dict[str, Optional[dict]]):
+            for locN, dataN in loc_keys.items():
+                jp_name = (packet.get('jp', {}).get(dataN) or
+                           packet.get('gl', {}).get(dataN))
+
+                for _, subpacket in packet:
+                    subpacket.pop(dataN)
+
+                packet.setdefault('loc', {})[dataN] = get_loc_for(f'SRPG_{group}Param_{key}_{locN}', jp_name)
+
+            buf = StringIO()
+            buf.write('{{#invoke:Data|insert')
+            for server in sorted(packet.keys()):
+                val = json.dumps(packet[server],
+                                 sort_keys=True,
+                                 ensure_ascii=False,
+                                 separators=(',', ':'))
+                buf.write(f'|{server}=<nowiki>{val}</nowiki>')
+            buf.write('}}')
+            to_return[f'Data:Game/MasterParam/{type_}/{k}'] = buf.getvalue()
+
+        replacements = {
+            'Unit': {
+                'NAME': 'name',
+            },
+            'Job': {
+                'NAME': 'name',
+                'CHARADESC': 'desc_ch',
+                'OTHERDESC': 'desc_ot',
+            },
+            'JobSet': {},
+            'JobGroup': {},
+            'UnitGroup': {},
+            'ConceptCardGroup': {},
+            'CustomTarget': {},
+            'ConceptCardConditions': {},
+            'Ability': {
+                'NAME': 'name',
+                'EXPR': 'expr',
+            },
+            'Buff': {},
+            'Cond': {},
+            'AI': {},
+            'Artifact': {
+                'NAME': 'name',
+                'EXPR': 'expr',
+                'SPEC': 'spec',
+                'FLAVOR': 'flavor',
+            },
+            'Item': {
+                'NAME': 'name',
+                'EXPR': 'expr',
+                'FLAVOR': 'flavor',
+            },
+        }
+        for type_, locKeys in replacements.items():
+            for k, packet in self.subdivide(groups.pop(type_), get_iname).items():
+                store_packet(type_, locKeys, k, packet)
+
+        # ConceptCard
+        trust_rewards = self.subdivide(groups.pop('ConceptCardTrustReward'), get_iname)
+        for k, packet in self.subdivide(groups.pop('ConceptCard'), get_iname).items():
+            for server, p in packet.items():
+                if p.get('trust_reward') is None: continue
+                trust_reward = trust_rewards.get(p.pop('trust_reward'))
+                if trust_reward is None: continue
+                rewards_parsed = OrderedDict()
+                for reward in trust_reward.get(server, {}).get('rewards', {}):
+                    if reward['reward_type'] in (1,2):
+                        m = rewards_parsed.setdefault(reward['reward_type'], OrderedDict())
+                        m[reward['reward_iname']] = m.get(reward['reward_iname'], 0) + reward['reward_num']
+                    else:
+                        raise NotImplementedError(f"Encountered unimplemented trust reward type: {json.dumps(reward)}")
+                for s, i in {'item': 1, 'artifact': 2}.items():
+                    key = f'trust_{s}_names'
+                    if key in p:
+                        raise ValueError(f'{key} already in {p}')
+                    p[key] = list(rewards_parsed.get(i, {}).keys())
+                    key = f'trust_{s}_names'
+                    if key in p:
+                        raise ValueError(f'{key} already in {p}')
+                    p[key] = list(rewards_parsed.get(i, {}).values())
+            store_packet('ConceptCard', {
+                'NAME': 'name',
+                'EXPR': 'expr',
+                'FLAVOR': 'flavor',
+            }, k, packet)
+
+        # Skill
+
+
+
+
+
+    @staticmethod
+    def subdivide(server_map: Mapping[str, Optional[Any]],
+                  f: Callable[[Any, Optional[Any]], str]) -> Dict[str, Dict[str, Optional[Any]]]:
+        groups = {}
+        for server, d in server_map.items():
+            for k, v in d.items():
+                groups.setdefault(f(k, v), {})[server] = v
+        return groups
+
+
+if __name__ == '__main__':
+    main()
     # curs = conn.cursor()
     # curs.execute('''
     #     SELECT f.file_path FROM projects AS p, files AS f
